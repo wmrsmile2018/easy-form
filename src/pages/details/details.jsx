c983637ed2e160ff40c07ef815c9875ab7150ed5
wmrsmile2018 <wmrsmile2018@gmail.com>
2021-09-09 21:10:13 +0300

    completed details page

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,22 +1,117 @@
-import React from 'react'
-import clsx  from 'clsx'
-import PropTypes from 'prop-types'
+import React, { useReducer, useMemo } from "react";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import key from "weak-key";
 
-import './details.scss'
-import { Title } from '../../components/title'
+import "./details.scss";
+import { Title } from "../../components/title";
+import { MarginGroup } from "../../components/marginGroup/marginGroup";
 
-export const Details = ({ className}) => {
-	const classes = clsx('about', className)
-	return <div className={classes}>
-		<Title>
-			Информация о мероприятии
-		</Title>
+const Resources = React.memo(({ className, resources }) => {
+  const classes = clsx("resources", className);
 
-<div className='about-header'>
-	Здесь будут основные данные
-</div>
-		<div className='about-table'>
-Здесь будет таблица
-		</div>
-	</div>
-}
+  return (
+    <div classes={classes}>
+      <div className="resources-header">
+        <span className="row__cell header__row-cell row__index">№</span>
+        <span className="row__cell header__row-cell row__url">Внешние ресурсы</span>
+        <span className="row__cell header__row-cell row__scheduled">
+          Запланированное количество человек
+        </span>
+        <span className="row__cell header__row-cell row__team">Перешеднее количество человек</span>
+      </div>
+      <div className="resources-content">
+        {resources.map((el, i) => (
+          <div className="row-cells" key={key(el)}>
+            <span className="row__cell row__index">{i + 1}</span>
+            <span className="row__cell row__url">{el.url}</span>
+            <span className="row__cell row__scheduled">{el.people_count}</span>
+            <span className="row__cell row__team">{el.came_people_count}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+});
+
+const Row = React.memo(
+  ({ className, qrUrl, peopleCount, team, index, rsrcCount, resources, qrPath }) => {
+    const [toggle, dispatch] = useReducer((state) => !state, false);
+    const classes = clsx("row", className, { showDetails: toggle });
+    const height = useMemo(() => (toggle ? (resources.length + 1) * 51 : 0), [toggle]);
+    return (
+      <div className={classes}>
+        <div className="row-cells__wrapper" onClick={dispatch}>
+          <div className="row-cells">
+            <span className="row__cell row__index">{index + 1}</span>
+            <span className="row__cell row__url">{qrUrl}</span>
+            <span className="row__cell row__team">{team ? "Да" : "Нет"}</span>
+            <span className="row__cell row__people-count">{peopleCount}</span>
+            <span className="row__cell row__rsrc-count">{rsrcCount}</span>
+            <span className="row__cell row__rsrc-count">{rsrcCount}</span>
+            <img className="row__cell row__qr-path" src={qrPath.path} alt="qr code" />
+          </div>
+        </div>
+        <div className="row-resources" style={{ height }}>
+          <Resources resources={resources} />
+        </div>
+      </div>
+    );
+  },
+);
+
+export const Details = React.memo(({ className, event }) => {
+  const { name, city, date, area, peopleCount, qrs } = event;
+  const classes = clsx("details", className);
+  return (
+    <div className={classes}>
+      <Title>Информация о мероприятии</Title>
+
+      <MarginGroup isColumn gap={20}>
+        <MarginGroup className="details-header" isColumn gap={5}>
+          <p>
+            Наименование мероприятия:<span>{name}</span>
+          </p>
+          <p>
+            Город:<span>{city}</span>
+          </p>
+          <p>
+            Дата:<span>{date}</span>
+          </p>
+          <p>
+            Место:<span>{area}</span>
+          </p>
+          <p>
+            Пришло людей:<span>{peopleCount}</span>
+          </p>
+        </MarginGroup>
+
+        <div className="details-table">
+          <div className="details-table-header">
+            <span className="row__cell header__row-cell row__index">№</span>
+            <span className="row__cell header__row-cell row__url">Url QR кода</span>
+            <span className="row__cell header__row-cell row__team">Командный QR</span>
+            <span className="row__cell header__row-cell row__people-count">Перешло людей</span>
+            <span className="row__cell header__row-cell row__rsrc-count">
+              Количество внешних ресурсов
+            </span>
+          </div>
+          <div className="details-table-content">
+            {qrs.map((el, i) => (
+              <Row
+                key={key(el)}
+                index={i}
+                qrUrl={el.qr_url}
+                peopleCount={el.people_count}
+                team={el.team}
+                rsrcCount={el.resources.length}
+                resources={el.resources}
+                qrPath={el.qrPath}
+              />
+            ))}
+          </div>
+        </div>
+      </MarginGroup>
+    </div>
+  );
+});
--- src/pages/details/detailsController.jsx
+++ src/pages/details/detailsController.jsx
@@ -1,6 +1,82 @@
-import React from 'react'
-import { Details } from './details'
+import React from "react";
+import { Details } from "./details";
 
-export const DetailsController = ({ className}) => {
-return <Details/>
-}
+export const DetailsController = ({ className }) => {
+  return (
+    <Details
+      event={{
+        name: "asadad",
+        city: "asdadadad",
+        date: "sdsadasd",
+        area: "asdasdasdad",
+        peopleCount: "sdgsfafsaf",
+        qrs: [
+          {
+            "id": "236",
+            "qr_suffix": "troparevo",
+            "event_id": "164",
+            "people_count": "0",
+            "qr_url": "http:localhost/troparevo",
+            "team": "true",
+            "deleted": "false",
+            "qrPath": {
+              "id": "141",
+              "qr_id": "236",
+              "path": "QRs/Поход/troparevo.png",
+              "deleted": "false",
+            },
+            "resources": [
+              {
+                "id": "374",
+                "qr_id": "236",
+                "qr_suffix": "troparevo",
+                "infinity": "false",
+                "url": "https://geekbrains.ru/certificates/688426#",
+                "people_count": "0",
+                "came_people_count": "0",
+                "deleted": "false",
+              },
+              {
+                "id": "375",
+                "qr_id": "236",
+                "qr_suffix": "troparevo",
+                "infinity": "true",
+                "url": "https://geekbrains.ru/certificates/681154",
+                "people_count": "0",
+                "came_people_count": "0",
+                "deleted": "false",
+              },
+            ],
+          },
+          {
+            "id": "237",
+            "qr_suffix": "bitcevskiy",
+            "event_id": "164",
+            "people_count": "0",
+            "qr_url": "http:localhost/bitcevskiy",
+            "team": "false",
+            "deleted": "false",
+            "qrPath": {
+              "id": "142",
+              "qr_id": "237",
+              "path": "QRs/Поход/bitcevskiy.png",
+              "deleted": "false",
+            },
+            "resources": [
+              {
+                "id": "376",
+                "qr_id": "237",
+                "qr_suffix": "bitcevskiy",
+                "infinity": "false",
+                "url": "https://geekbrains.ru/certificates/762129",
+                "people_count": "0",
+                "came_people_count": "0",
+                "deleted": "false",
+              },
+            ],
+          },
+        ],
+      }}
+    />
+  );
+};
