26728cb434dad9dad8496d0d0f899860093d55b0
Anh Nguyen <TkNguen@sberbank.ru>
2022-02-15 17:00:48 +0300

    returned button Exit

--- src/App.jsx
+++ src/App.jsx
@@ -17,52 +17,63 @@ const ErrorComponent = ({ status }) => {
   return <div className="error-component">Current page doesn't exist</div>;
 };
 
-export const Router = () => (
-  <>
-    <div className="app-navigation">
-      <div className="app-navigation-wrapper">
-        <NavLink exact className="app__nav-link" activeClassName="activeRoute" to="/admin">
-          Главная
-        </NavLink>
-        <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/basket">
-          Корзина
-        </NavLink>
-        <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/add-event">
-          Добавить мероприятие
-        </NavLink>
-        <NavLink
-          className="app__nav-link"
-          activeClassName="activeRoute"
-          to="/admin/get-info-default-resource"
-        >
-          Общий дефолтный <br />
-          внешний ресурс
-        </NavLink>
-        <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/FAQ">
-          FAQ
-        </NavLink>
-        <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/users">
-          Администрирование
-        </NavLink>
+export const Router = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const handleOnLogout = () => {
+    history.push("/admin/sign-in");
+    dispatch({ type: "CLEAR_STORE" });
+  };
+  return (
+    <>
+      <div className="app-navigation">
+        <div className="app-navigation-wrapper">
+          <NavLink exact className="app__nav-link" activeClassName="activeRoute" to="/admin">
+            Главная
+          </NavLink>
+          <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/basket">
+            Корзина
+          </NavLink>
+          <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/add-event">
+            Добавить мероприятие
+          </NavLink>
+          <NavLink
+            className="app__nav-link"
+            activeClassName="activeRoute"
+            to="/admin/get-info-default-resource"
+          >
+            Общий дефолтный <br />
+            внешний ресурс
+          </NavLink>
+          <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/FAQ">
+            FAQ
+          </NavLink>
+          <NavLink className="app__nav-link" activeClassName="activeRoute" to="/admin/users">
+            Администрирование
+          </NavLink>
+          <NavLink className="app__nav-link" activeClassName="activeRoute" to="/sign-in">
+            <p onClick={handleOnLogout}>Выйти</p>
+          </NavLink>
+        </div>
       </div>
-    </div>
-    <main id="main">
-      <div className="app-content">
-        <Switch>
-          <Route exact path="/admin" component={EventsController} />
-          <Route path="/admin/add-event" component={AddEventController} />
-          <Route path="/admin/users" component={UsersController} />
-          <Route path="/admin/edit-event/:id" component={EditEventController} />
-          <Route path="/admin/basket" component={DeletedEventsController} />
-          <Route path="/admin/details/:id" component={DetailsController} />
-          <Route path="/admin/get-info-default-resource" component={DefaultResourceCotroller} />
-          <Route path="/admin/FAQ" component={AboutUs} />
-          <Route path="*" component={ErrorComponent} />
-        </Switch>
-      </div>
-    </main>
-  </>
-);
+      <main id="main">
+        <div className="app-content">
+          <Switch>
+            <Route exact path="/admin" component={EventsController} />
+            <Route path="/admin/add-event" component={AddEventController} />
+            <Route path="/admin/users" component={UsersController} />
+            <Route path="/admin/edit-event/:id" component={EditEventController} />
+            <Route path="/admin/basket" component={DeletedEventsController} />
+            <Route path="/admin/details/:id" component={DetailsController} />
+            <Route path="/admin/get-info-default-resource" component={DefaultResourceCotroller} />
+            <Route path="/admin/FAQ" component={AboutUs} />
+            <Route path="*" component={ErrorComponent} />
+          </Switch>
+        </div>
+      </main>
+    </>
+  );
+};
 
 function App() {
   const token = useSelector((state) => state.auth.token);
